bbe375c7e6fedaae9f6a83a76bedb4266f0ca529
Hans Pagel <mail@hanspagel.com>
2024-01-31 13:53:00 +0100

    chore: browser build (wip)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vitest/config'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig({
-  plugins: [],
+  plugins: [
+    // TODO: Doesn’t even work
+    // https://github.com/ajv-validator/ajv/issues/1744
+    nodePolyfills({
+      // To exclude specific polyfills, add them to this list.
+      exclude: [],
+      // Whether to polyfill specific globals.
+      globals: {
+        Buffer: true, // can also be 'build', 'dev', or false
+        global: true,
+        process: true,
+      },
+      // Whether to polyfill `node:` protocol imports.
+      protocolImports: true,
+    }),
+  ],
   build: {
     lib: {
       entry: ['src/index.ts'],
