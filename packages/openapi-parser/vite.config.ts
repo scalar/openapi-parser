1861b05db489973ed8397f6aa6f099f14df4e214
Hans Pagel <mail@hanspagel.com>
2024-02-08 16:12:06 +0100

    chore: add type definitions

--- packages/openapi-parser/vite.config.ts
+++ packages/openapi-parser/vite.config.ts
@@ -1,15 +1,13 @@
+import dts from 'vite-plugin-dts'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  plugins: [],
+  plugins: [dts({ rollupTypes: true })],
   build: {
     lib: {
       entry: ['src/index.ts'],
       name: '@scalar/openapi-parser',
       formats: ['es'],
-    },
-    rollupOptions: {
-      external: ['@seriousme/openapi-schema-validator'],
     },
   },
   test: {
