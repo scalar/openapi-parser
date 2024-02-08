20e85eba2317cb544ccb2bd172b22b894097c8e5
Hans Pagel <mail@hanspagel.com>
2024-02-08 15:05:08 +0100

    test: only test 10 files in CI (wip)

--- packages/openapi-parser/tests/files.test.ts
+++ packages/openapi-parser/tests/files.test.ts
@@ -7,16 +7,22 @@ import { parse } from '../src'
 const invalidFiles = [
   'packages/openapi-parser/tests/files/statsocialcom.yaml',
   'packages/openapi-parser/tests/files/spotifycom.yaml',
+  'packages/openapi-parser/tests/files/opensuseorgobs.yaml',
+  'packages/openapi-parser/tests/files/royalmailcomclick-and-drop.yaml',
 ]
 
 const files = (
   await glob('./packages/openapi-parser/tests/files/*.yaml')
 ).filter((file) => !invalidFiles.includes(file))
 
+/**
+ * This test suite parses a large number of real-world OpenAPI files
+ */
 describe.sequential('files:parse', async () => {
   // Those tests take a while, let’s run them in CI only.
-  if (!process.env.CI) {
-    test.each(files.slice(0, 300))('[%s] parse', async (file) => {
+  if (process.env.CI) {
+    // TODO: We’re currently only testing a few of the files for performance reasons.
+    test.each(files.slice(0, 10))('[%s] parse', async (file) => {
       const content = fs.readFileSync(file, 'utf-8')
       const result = await parse(content)
 
