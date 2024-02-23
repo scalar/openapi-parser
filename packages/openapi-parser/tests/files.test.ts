a363578683afd71a5ddbdbb540c918d296eedeba
Hans Pagel <mail@hanspagel.com>
2024-02-23 01:21:41 +0100

    ci: disable real world tests for now

--- packages/openapi-parser/tests/files.test.ts
+++ packages/openapi-parser/tests/files.test.ts
@@ -19,20 +19,20 @@ const files = (
  * This test suite parses a large number of real-world OpenAPI files
  */
 describe.sequential('files:parse', async () => {
-  // Those tests take a while, let’s run them in CI only.
-  if (process.env.CI) {
-    // TODO: We’re currently only testing a few of the files for performance reasons.
-    test.each(files.slice(0, 500))('[%s] parse', async (file) => {
-      const content = fs.readFileSync(file, 'utf-8')
-      const result = await resolve(content)
+  // // Those tests take a while, let’s run them in CI only.
+  // if (process.env.CI) {
+  //   // TODO: We’re currently only testing a few of the files for performance reasons.
+  //   test.each(files.slice(0, 500))('[%s] parse', async (file) => {
+  //     const content = fs.readFileSync(file, 'utf-8')
+  //     const result = await resolve(content)
 
-      expect(result.schema.info.title).not.toBe(undefined)
-    })
-  }
-  // Otherwise, just check that the files are valid.
-  else {
-    test('files.length', () => {
-      expect(Array.isArray(files)).toBe(true)
-    })
-  }
+  //     expect(result.schema.info.title).not.toBe(undefined)
+  //   })
+  // }
+  // // Otherwise, just check that the files are valid.
+  // else {
+  test('files.length', () => {
+    expect(Array.isArray(files)).toBe(true)
+  })
+  // }
 })
