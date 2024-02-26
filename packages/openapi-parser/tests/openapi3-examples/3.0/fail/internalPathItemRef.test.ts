2b559980a2733cd2895600826bb6b7e1b6c22a13
Hans Pagel <mail@hanspagel.com>
2024-02-26 16:36:55 +0100

    tests: enable some more tests

--- packages/openapi-parser/tests/openapi3-examples/3.0/fail/internalPathItemRef.test.ts
+++ packages/openapi-parser/tests/openapi3-examples/3.0/fail/internalPathItemRef.test.ts
@@ -3,17 +3,11 @@ import { describe, expect, it } from 'vitest'
 import { resolve } from '../../../../src'
 import internalPathItemRef from './internalPathItemRef.yaml'
 
-describe.todo('internalPathItemRef', () => {
+describe('internalPathItemRef', () => {
   it('returns an error', async () => {
     const result = await resolve(internalPathItemRef)
 
-    // TODO: Swagger Editor
-    //
-    // Resolver error at paths./test.$ref
-    // Could not resolve reference: undefined undefined
-    expect(result.errors?.[0]?.error).toBe(
-      `something something internal path item ref`,
-    )
+    expect(result.errors?.[0]?.error).toBe(`Can’t resolve $ref: #/paths/test2`)
     expect(result.errors?.length).toBe(1)
     expect(result.valid).toBe(false)
   })
