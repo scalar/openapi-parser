ad354ba9294b0c90c99823beae5214e3cbeda670
Hans Pagel <mail@hanspagel.com>
2024-02-28 01:16:17 +0100

    chore: focus on one diff

--- packages/openapi-parser/tests/diff.test.ts
+++ packages/openapi-parser/tests/diff.test.ts
@@ -7,22 +7,23 @@ import { describe, expect, test } from 'vitest'
 import { normalize, openapi } from '../src'
 import type { AnyObject } from '../src'
 
-const invalidFiles = [
-  'packages/openapi-parser/tests/files/statsocialcom.yaml',
-  'packages/openapi-parser/tests/files/spotifycom.yaml',
-  'packages/openapi-parser/tests/files/opensuseorgobs.yaml',
-  'packages/openapi-parser/tests/files/royalmailcomclick-and-drop.yaml',
-  // max call stack size exceeded
-  'packages/openapi-parser/tests/files/xerocomxero_accounting.yaml',
-  'packages/openapi-parser/tests/files/xtrfeu.yaml',
-  'packages/openapi-parser/tests/files/webflowcom.yaml',
-  'packages/openapi-parser/tests/files/airport-webappspotcom.yaml',
-  'packages/openapi-parser/tests/files/amazonawscomathena.yaml',
-]
+// const invalidFiles = [
+//   'packages/openapi-parser/tests/files/statsocialcom.yaml',
+//   'packages/openapi-parser/tests/files/spotifycom.yaml',
+//   'packages/openapi-parser/tests/files/opensuseorgobs.yaml',
+//   'packages/openapi-parser/tests/files/royalmailcomclick-and-drop.yaml',
+//   // max call stack size exceeded
+//   'packages/openapi-parser/tests/files/xerocomxero_accounting.yaml',
+//   'packages/openapi-parser/tests/files/xtrfeu.yaml',
+//   'packages/openapi-parser/tests/files/webflowcom.yaml',
+//   'packages/openapi-parser/tests/files/airport-webappspotcom.yaml',
+//   'packages/openapi-parser/tests/files/amazonawscomathena.yaml',
+// ]
 
-const files = (await glob('./packages/openapi-parser/tests/files/*.yaml'))
-  .filter((file) => !invalidFiles.includes(file))
-  .sort()
+// const files = (await glob('./packages/openapi-parser/tests/files/*.yaml'))
+//   .filter((file) => !invalidFiles.includes(file))
+//   .sort()
+const files = ['packages/openapi-parser/tests/files/1passwordcomevents.yaml']
 
 /**
  * This test suite parses a large number of real-world OpenAPI files
@@ -31,21 +32,24 @@ describe('diff', async () => {
   // TODO: We’re currently only testing a few of the files for performance reasons.
   test.each(files.slice(0, 100))('[%s] diff', async (file) => {
     const content = fs.readFileSync(file, 'utf-8')
-    const specification: object = normalize(content)
+    const specification = normalize(content)
 
     const oldSchema = (await new Promise((resolve, reject) => {
-      SwaggerParser.dereference(specification as never, (error, result) => {
-        if (error) {
-          reject(error)
-        }
+      SwaggerParser.dereference(
+        structuredClone(specification) as never,
+        (error, result) => {
+          if (error) {
+            reject(error)
+          }
 
-        if (result === undefined) {
-          reject('Couldn’t parse the Swagger file.')
+          if (result === undefined) {
+            reject('Couldn’t parse the Swagger file.')
 
-          return
-        }
-        resolve(result)
-      })
+            return
+          }
+          resolve(result)
+        },
+      )
     }).catch((error) => {
       console.error('[@apidevtools/swagger-parser]', error)
     })) as any
@@ -54,28 +58,34 @@ describe('diff', async () => {
       schema: newSchema,
       valid,
       errors,
-    } = await openapi().load(specification).resolve()
+    } = await openapi().load(structuredClone(specification)).resolve()
 
-    // Valid?
-    if (!valid) {
-      console.log(errors)
-    }
-    expect(valid).toBe(true)
-
-    // Same number of paths?
-    expect(Object.keys(oldSchema.paths ?? {}).length).toEqual(
-      Object.keys(newSchema.paths ?? {}).length,
+    console.log(
+      specification.paths['/api/v1/itemusages'].post.requestBody,
+      oldSchema.paths['/api/v1/itemusages'].post.requestBody,
+      newSchema.paths['/api/v1/itemusages'].post.requestBody,
     )
 
-    // Any difference?
-    const result = diff(oldSchema, newSchema)
-    expect(result).toEqual([])
+    // // Valid?
+    // if (!valid) {
+    //   console.log(errors)
+    // }
+    // expect(valid).toBe(true)
 
-    if (result.length) {
-      result.forEach(({ op, path }) => {
-        console.log(op, get(specification, path))
-      })
-    }
+    // // Same number of paths?
+    // expect(Object.keys(oldSchema.paths ?? {}).length).toEqual(
+    //   Object.keys(newSchema.paths ?? {}).length,
+    // )
+
+    // // Any difference?
+    // const result = diff(oldSchema, newSchema)
+    // expect(result).toEqual([])
+
+    // if (result.length) {
+    //   result.forEach(({ op, path }) => {
+    //     console.log(op, get(specification, path))
+    //   })
+    // }
   })
 })
 
