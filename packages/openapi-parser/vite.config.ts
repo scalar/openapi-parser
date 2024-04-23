0eb2aaf416b497a6977e8f46b5f46220bca5afed
Hans Pagel <mail@hanspagel.com>
2024-04-23 13:23:59 +0200

    chore: add relative ci

--- packages/openapi-parser/vite.config.ts
+++ packages/openapi-parser/vite.config.ts
@@ -1,9 +1,10 @@
 import ViteYaml from '@modyfi/vite-plugin-yaml'
+import { webpackStats } from 'rollup-plugin-webpack-stats'
 import dts from 'vite-plugin-dts'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  plugins: [ViteYaml(), dts({ rollupTypes: true })],
+  plugins: [ViteYaml(), dts({ rollupTypes: true }), webpackStats()],
   build: {
     lib: {
       entry: ['src/index.ts'],
