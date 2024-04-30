67cb1fdc7a0196ab0b5fe70dc32e2f650a5bbac5
Hans Pagel <mail@hanspagel.com>
2024-04-30 16:22:42 +0200

    chore: enable failing test

--- packages/openapi-parser/tests/openapi3-examples/3.0/pass/externalPathItemRef.test.ts
+++ packages/openapi-parser/tests/openapi3-examples/3.0/pass/externalPathItemRef.test.ts
@@ -1,16 +1,17 @@
+import path from 'node:path'
 import { describe, expect, it } from 'vitest'
 
 import { loadFiles, resolve } from '../../../../src'
-import { relativePath } from '../../../../tests/utils'
 
-const EXAMPLE_FILE = relativePath(
-  import.meta.url,
-  './pass/externalPathItemRef.yaml',
+const EXAMPLE_FILE = path.join(
+  new URL(import.meta.url).pathname,
+  '../../pass/externalPathItemRef.yaml',
 )
 
-describe.todo('externalPathItemRef', () => {
+describe('externalPathItemRef', () => {
   it('passes', async () => {
     const filesystem = loadFiles(EXAMPLE_FILE)
+
     const result = await resolve(filesystem)
 
     expect(result.valid).toBe(true)
