4c2c72e647920bb1a166b164fa28571b043fc247
Hans Pagel <mail@hanspagel.com>
2024-05-02 15:11:14 +0200

    chore: improve error message when URIs can’t be found

--- packages/openapi-parser/src/lib/Validator/resolveReferences.ts
+++ packages/openapi-parser/src/lib/Validator/resolveReferences.ts
@@ -150,7 +150,13 @@ function resolveUri(
   // Pointers
   const segments = unescapeJsonPointer(path).split('/').slice(1)
 
-  return segments.reduce((acc, key) => {
-    return acc[key]
-  }, file.specification)
+  try {
+    return segments.reduce((acc, key) => {
+      return acc[key]
+    }, file.specification)
+  } catch (error) {
+    // TODO: Move to error messages
+    const URI_NOT_FOUND = 'Can’t resolve URI: %s'
+    throw new Error(URI_NOT_FOUND.replace('%s', uri))
+  }
 }
