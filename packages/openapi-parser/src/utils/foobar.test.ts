c3acf06ea0c4d7ff163b5da1f4e78cdb8c686149
Hans Pagel <mail@hanspagel.com>
2024-05-13 15:47:09 +0200

    feat: add url plugin

--- packages/openapi-parser/src/utils/foobar.test.ts
+++ packages/openapi-parser/src/utils/foobar.test.ts
@@ -3,36 +3,39 @@ import { describe, expect, it } from 'vitest'
 import YAML from 'yaml'
 
 import { getEntrypoint } from './getEntrypoint'
-import { load, loadFilesPlugin } from './load'
+import { fetchUrlsPlugin, load, readFilesPlugin } from './load'
 
 describe('isFile', async () => {
-  it.only('returns true for a file name', async () => {
-    expect(loadFilesPlugin.check('openapi.yaml')).toBe(true)
+  it('returns true for a file name', async () => {
+    expect(readFilesPlugin.check('openapi.yaml')).toBe(true)
   })
 
-  it.only('returns true for a path', async () => {
-    expect(loadFilesPlugin.check('specification/openapi.yaml')).toBe(true)
+  it('returns true for a path', async () => {
+    expect(readFilesPlugin.check('specification/openapi.yaml')).toBe(true)
   })
 
-  it.only('returns false for an object', async () => {
-    expect(loadFilesPlugin.check({})).toBe(false)
+  it('returns false for an object', async () => {
+    expect(readFilesPlugin.check({})).toBe(false)
   })
 
-  it.only('returns false for undefinded', async () => {
-    expect(loadFilesPlugin.check()).toBe(false)
+  it('returns false for undefinded', async () => {
+    expect(readFilesPlugin.check()).toBe(false)
   })
 
-  it.only('returns false for an url', async () => {
+  it('returns false for an url', async () => {
     expect(
-      loadFilesPlugin.check('http://example.com/specification/openapi.yaml'),
+      readFilesPlugin.check('http://example.com/specification/openapi.yaml'),
     ).toBe(false)
   })
 })
 
 describe('load', async () => {
-  it.only('loads file', async () => {
+  it('loads file', async () => {
     const filesystem = await load(
       path.join(new URL(import.meta.url).pathname, '../examples/openapi.yaml'),
+      {
+        plugins: [readFilesPlugin, fetchUrlsPlugin],
+      },
     )
 
     expect(getEntrypoint(filesystem).specification).toMatchObject({
@@ -46,7 +49,22 @@ describe('load', async () => {
   })
 
   it('fetches url', async () => {
-    const filesystem = await load('https://example.com/openapi.yaml')
+    // @ts-expect-error only partially patched
+    global.fetch = async () => ({
+      text: async () =>
+        YAML.stringify({
+          openapi: '3.1.0',
+          info: {
+            title: 'Hello World',
+            version: '1.0.0',
+          },
+          paths: {},
+        }),
+    })
+
+    const filesystem = await load('https://example.com/openapi.yaml', {
+      plugins: [readFilesPlugin, fetchUrlsPlugin],
+    })
 
     expect(getEntrypoint(filesystem).specification).toMatchObject({
       openapi: '3.1.0',
@@ -59,14 +77,19 @@ describe('load', async () => {
   })
 
   it('retrieves JS object', async () => {
-    const filesystem = await load({
-      openapi: '3.1.0',
-      info: {
-        title: 'Hello World',
-        version: '1.0.0',
+    const filesystem = await load(
+      {
+        openapi: '3.1.0',
+        info: {
+          title: 'Hello World',
+          version: '1.0.0',
+        },
+        paths: {},
       },
-      paths: {},
-    })
+      {
+        plugins: [readFilesPlugin, fetchUrlsPlugin],
+      },
+    )
 
     expect(getEntrypoint(filesystem).specification).toMatchObject({
       openapi: '3.1.0',
@@ -88,6 +111,9 @@ describe('load', async () => {
         },
         paths: {},
       }),
+      {
+        plugins: [readFilesPlugin, fetchUrlsPlugin],
+      },
     )
 
     expect(getEntrypoint(filesystem).specification).toMatchObject({
@@ -110,6 +136,9 @@ describe('load', async () => {
         },
         paths: {},
       }),
+      {
+        plugins: [readFilesPlugin, fetchUrlsPlugin],
+      },
     )
 
     expect(getEntrypoint(filesystem).specification).toMatchObject({
--- packages/openapi-parser/src/utils/load.ts
+++ packages/openapi-parser/src/utils/load.ts
@@ -1,4 +1,5 @@
 import { existsSync, readFileSync } from 'node:fs'
+import YAML from 'yaml'
 
 import { ERRORS } from '../configuration'
 import { makeFilesystem } from './makeFilesystem'
@@ -8,13 +9,30 @@ export type LoadPlugin = {
   get: (value: any) => any
 }
 
-export const loadFilesPlugin: LoadPlugin = {
+export const readFilesPlugin: LoadPlugin = {
   check(value?: any) {
+    // Not a string
     if (typeof value !== 'string') {
       return false
     }
 
+    // URL
     if (value.startsWith('http://') || value.startsWith('https://')) {
+      return false
+    }
+
+    // Line breaks
+    if (value.includes('\n')) {
+      return false
+    }
+
+    // JSON
+    if (isJson(value)) {
+      return false
+    }
+
+    // YAML (run through YAML.parse)
+    if (isYaml(value)) {
       return false
     }
 
@@ -28,22 +46,74 @@ export const loadFilesPlugin: LoadPlugin = {
     try {
       return readFileSync(value, 'utf-8')
     } catch (error) {
-      console.error('[loadFilesPlugin]', error)
+      console.error('[readFilesPlugin]', error)
     }
   },
 }
 
-// TODO: Make configurable
-const plugins: LoadPlugin[] = [loadFilesPlugin]
+export function isJson(value: string) {
+  try {
+    JSON.parse(value)
 
-export async function load(value: any) {
-  for (const plugin of plugins) {
+    return true
+  } catch {
+    return false
+  }
+}
+
+export function isYaml(value: string) {
+  // Line breaks
+  if (!value.includes('\n')) {
+    return false
+  }
+
+  try {
+    YAML.parse(value)
+    return true
+  } catch (error) {
+    console.log('ADSAS', error)
+    return false
+  }
+}
+
+export const fetchUrlsPlugin: LoadPlugin = {
+  check(value?: any) {
+    // Not a string
+    if (typeof value !== 'string') {
+      return false
+    }
+
+    // Not http/https
+    if (!value.startsWith('http://') && !value.startsWith('https://')) {
+      return false
+    }
+
+    return true
+  },
+  async get(value?: any) {
+    try {
+      const response = await fetch(value)
+
+      return await response.text()
+    } catch (error) {
+      console.error('[fetchUrlsPlugin]', error)
+    }
+  },
+}
+
+export async function load(
+  value: any,
+  options?: {
+    plugins?: LoadPlugin[]
+  },
+) {
+  for (const plugin of options?.plugins ?? []) {
     if (plugin.check(value)) {
       const content = await plugin.get(value)
-
-      // TODO: Fetch references
 
       return makeFilesystem(content)
     }
   }
+
+  return makeFilesystem(value)
 }
