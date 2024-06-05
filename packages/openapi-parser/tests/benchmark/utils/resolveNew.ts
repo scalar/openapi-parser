a110ce0dbdb78575289acb51405fb0cd7e4a9ea0
Hans Pagel <mail@hanspagel.com>
2024-06-05 16:21:13 +0200

    fix: benchmarks

--- packages/openapi-parser/tests/benchmark/utils/resolveNew.ts
+++ packages/openapi-parser/tests/benchmark/utils/resolveNew.ts
@@ -1,9 +1,4 @@
-import {
-  type AnyObject,
-  normalize,
-  openapi,
-  resolveReferences,
-} from '../../../src'
+import { type AnyObject, normalize, resolveReferences } from '../../../src'
 
 export async function resolveNew(specification: AnyObject) {
   return resolveReferences(normalize(specification))
