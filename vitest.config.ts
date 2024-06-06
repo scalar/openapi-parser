86bd47649707b7d8ecf380372fe546b9c59f62aa
Hans Pagel <mail@hanspagel.com>
2024-06-06 14:28:27 +0200

    chore: add code coverage report

---
+++ vitest.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    coverage: {
+      enabled: true,
+      exclude: [
+        'demo/*',
+        'scripts/*',
+        'vitest.workspace.ts',
+        '**/createProfile.ts',
+        '**/*.bench.ts',
+      ],
+    },
+    reporters: ['default', 'json'],
+    outputFile: './results.json',
+  },
+})
