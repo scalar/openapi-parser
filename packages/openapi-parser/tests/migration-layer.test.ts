b8aadaabe7eafdd88868256eb67fcc3c3e1097a2
Hans Pagel <mail@hanspagel.com>
2024-06-21 14:19:44 +0200

    feat: add dereference to the migration layer

--- packages/openapi-parser/tests/migration-layer.test.ts
+++ packages/openapi-parser/tests/migration-layer.test.ts
@@ -1,16 +1,32 @@
 import OriginalSwaggerParser from '@apidevtools/swagger-parser'
 import { describe, expect, it } from 'vitest'
 
+import { dereference } from '../src/utils/dereference'
 import { validate } from '../src/utils/validate'
 
-const myAPI = `{
-    "openapi": "3.1.0",
-    "info": {
-        "title": "Hello World",
-        "version": "1.0.0"
+const myAPI = JSON.stringify({
+  openapi: '3.1.0',
+  info: {
+    title: 'Hello World',
+    version: '1.0.0',
+  },
+  paths: {
+    '/foobar': {
+      post: {
+        requestBody: {
+          $ref: '#/components/requestBodies/Foobar',
+        },
+      },
     },
-    "paths": {}
-}`
+  },
+  components: {
+    requestBodies: {
+      Foobar: {
+        content: {},
+      },
+    },
+  },
+})
 
 class SwaggerParser {
   static async validate(api: string, callback: (err: any, api: any) => void) {
@@ -24,10 +40,14 @@ class SwaggerParser {
         callback(error, null)
       })
   }
+
+  static async dereference(api: string) {
+    return dereference(api).then((result) => result.schema)
+  }
 }
 
 // https://github.com/APIDevTools/swagger-parser?tab=readme-ov-file#example
-describe('migration-layer', async () => {
+describe('validate', async () => {
   it('validates', async () => {
     return new Promise((resolve, reject) => {
       SwaggerParser.validate(myAPI, (err, api) => {
@@ -53,5 +73,16 @@ describe('migration-layer', async () => {
         }
       })
     })
+  })
+})
+
+// https://apitools.dev/swagger-parser/docs/swagger-parser.html#dereferenceapi-options-callback
+describe('dereference', () => {
+  it('dereferences', async () => {
+    let api = await SwaggerParser.dereference(myAPI)
+
+    // The `api` object is a normal JavaScript object,
+    // so you can easily access any part of the API using simple dot notation
+    expect(api?.paths?.['/foobar']?.post?.requestBody?.content).toEqual({})
   })
 })
